514cf583d3e96ae1444a7e977ef0e2afe7d38b43
Ahmed2Ayman <ahmedayman01067217949@gmail.com>
2023-04-27 12:42:17 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    );
+  }
+}
+
+
+
